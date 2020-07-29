fbe6916d7d59520c9ddb33c207e9487ac628c94d
zacharymollenhour <Zacharymmollenhour@gmail.com>
2020-07-29 13:19:33 -0400

    Changes to formula

--- Day1/main.cpp
+++ Day1/main.cpp
@@ -1,12 +1,25 @@
+// ****************************************************************************
+// * Name: main cpp file ( main.cpp filename )                                *
+// * Description: This file will assist in the calculating of savings interest*
+// * that can be acquired by putting money into a bank account for a set      *
+// * time frame                                                               *
+// * Author: Zachary Mollenhour                                               *
+// * Date: July 29th 2020 ( last modified )                                   *
+// ****************************************************************************
+
 #include <iostream>
 #include "savings.h"
 using namespace std;
 
 int main(void)
 {
-    Savings();
-    Savings saving1(100.0,1.5,200.00,20.0,5); //Default Constructor
-    saving1.Input(); //Get input for object
+    SavingsAccount person1(2000.00);
+    SavingsAccount person2(3000.00);
+
+    SavingsAccount::modifyInterestRate(3);
+
+    person1.calculateMonthlyInterest();
+    cout << "Person 1 Savings Balance: " << person1.GetBalance() << endl;
 
     return 0;
 }
--- Day1/savings.cpp
+++ Day1/savings.cpp
@@ -7,75 +7,24 @@
 // * Date: July 29th 2020 ( last modified )                                   *
 // ****************************************************************************
 #include "savings.h"
-void Savings:: Input()
+#include <iomanip>
+
+
+SavingsAccount:: SavingsAccount(int value)
 {
-    float balance,interest,depositAmount;
-    int monthsOpen;
-
-    //Input of user Balance
-    cout << "Please enter your current savings balance: ";
-    cin >> balance;
-
-    //Input of interest rate
-    cout << "\nPlease enter your expected interest rate: ";
-    cin >> interest;
-
-    //Input of Deposit Amount
-    cout << "\nPlease enter your expected recurring deposit amount (ex. 10.00): ";
-    cin >> depositAmount;
-    cout << "\nPlease enter the amount of months you want to calculate interest for (ex. 10): ";
-    cin >> monthsOpen;
-
-    //loop that continues untill the user enters valid data above
-    while(!Set(balance,interest,depositAmount,monthsOpen))
-    {
-        cout << "Please enter valid data. Try Again: " << endl;
-        cout << "Please enter your current savings balance: ";
-        cin >> balance;
-
-        //Input of interest rate
-        cout << "\nPlease enter your expected interest rate: ";
-        cin >> interest;
-
-        //Input of Deposit Amount
-        cout << "\nPlease enter your expected recurring deposit amount (ex. 10.00): ";
-        cin >> depositAmount;
-        cout << "\nPlease enter the amount of months you want to calculate interest for (ex. 10): ";
-        cin >> monthsOpen;
-    }
+    savingsBalance = value;
 }
 
-bool Savings:: Set(float balance,float interest,float depositAmount,int monthsOpen)
+float SavingsAccount :: annualInterestRate = 0;
+
+void SavingsAccount::calculateMonthlyInterest()
 {
-    //Check if data is valid
-    if(balance >= 0.0)
-    {
-        if(interest >= 0.0)
-        {
-            if(depositAmount >= 0.0)
-            {
-                if(monthsOpen >= 0)
-                {
-                    return true;
-                }
-                else{
-                    return false;
-                }
-            }
-            else{
-                return false;
-            }
-        }
-        else{
-            return false;
-        }
-    }
-    else{
-        return false;
-    }
+    savingsBalance += ((savingsBalance * annualInterestRate) / 12);
 }
 
-int Savings:: GetBalance() const
+void SavingsAccount::modifyInterestRate(float value)
 {
-    return User_Balance;
+    annualInterestRate = value;
 }
+
+
