621e24a9cf6642b1e22c3d26b0da22d457844a1e
zacharymollenhour <Zacharymmollenhour@gmail.com>
2020-08-04 12:12:38 -0400

    Init Commit

---
+++ Day3/InsertionSort.cpp
@@ -0,0 +1,10 @@
+// ****************************************************************************
+// * Name: Insertion Sort cpp file ( insertion.cpp filename )                      *
+// * Description: This file will perform a insertion sort algorithm on a array   *
+// * Author: Zachary Mollenhour                                               *
+// * Date: August 4th 2020 ( last modified )                                  *
+// ****************************************************************************
+
+#include <iostream>
+using namespace std;
+
