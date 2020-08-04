a76ea3b06b0f635ecf83561ecf165b4a2784fff0
zacharymollenhour <Zacharymmollenhour@gmail.com>
2020-08-04 11:59:08 -0400

    Init Commit

---
+++ Day3/BinarySort.cpp
@@ -0,0 +1,10 @@
+// ****************************************************************************
+// * Name: Binary Sort cpp file ( savings.cpp filename )                      *
+// * Description: This file will perform a binary sort algorithm on a array   *
+// * Author: Zachary Mollenhour                                               *
+// * Date: August 4th 2020 ( last modified )                                  *
+// ****************************************************************************
+
+#include <iostream>
+using namespace std;
+
